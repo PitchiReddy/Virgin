9897a378ce9c9e0b0355e2f04c1fc3b4b811931f
rpraveen <rpraveen@LIN66004745.corp.capgemini.com>
2017-09-12 10:50:12 +0530

    Removed unused code

--- applications/crossreference-api/src/test/java/com/virginvoyages/crossreference/helper/TestDataHelper.java
+++ applications/crossreference-api/src/test/java/com/virginvoyages/crossreference/helper/TestDataHelper.java
@@ -8,11 +8,16 @@ import com.virginvoyages.crossreference.references.Reference;
 import com.virginvoyages.crossreference.sources.ReferenceSource;
 import com.virginvoyages.crossreference.types.ReferenceType;
 
+/**
+ * Helper class for testcases
+ * @author rpraveen
+ *
+ */
 @Service
 public class TestDataHelper {
 
 	public ReferenceSource getDataForCreateReferenceSource() {
-		
+
 		return new ReferenceSource().auditData(createAuditDataForCreate()).referenceSourceID("RS30")
 				.referenceSourceName("Seaware");
 	}
@@ -26,25 +31,9 @@ public class TestDataHelper {
 
 	public Reference getDataForCreateReference() {
 
-		return new Reference()
-				.auditData(createAuditDataForCreate())
-				.referenceType(getDataForCreateReferenceType())
-				.referenceSource(getDataForCreateReferenceSource())
-				.referenceID("R30")
-				.details("Dummy Reference Entry 30")
-				.expiry(LocalDate.now())
-				.masterID("M30")
-				.nativeSourceID("NSID30");
-	}
-
-	public ReferenceType getReferenceTypeByID() {
-		ReferenceType referenceType = new ReferenceType();
-		Audited Audited = createAuditDataForCreate();
-		referenceType.auditData(Audited);
-		referenceType.referenceName("referenceName");
-		referenceType.referenceType("referenceType");
-		referenceType.referenceTypeID("RT1");
-		return referenceType;
+		return new Reference().auditData(createAuditDataForCreate()).referenceType(getDataForCreateReferenceType())
+				.referenceSource(getDataForCreateReferenceSource()).referenceID("R30")
+				.details("Dummy Reference Entry 30").expiry(LocalDate.now()).masterID("M30").nativeSourceID("NSID30");
 	}
 
 	public Audited createAuditDataForCreate() {
@@ -57,51 +46,4 @@ public class TestDataHelper {
 		return audited;
 
 	}
-
-	public String deleteReferenceTypeByID() {
-
-		return "RT1";
-	}
-
-	public Reference getReferenceByID() {
-		Reference reference = new Reference();
-		Audited audited = createAuditDataForCreate();
-		reference.auditData(audited);
-		ReferenceSource referenceSource = creatreferenceSourceDataForCreate();
-		reference.referenceSource(referenceSource);
-		ReferenceType referenceType = creatreferenceTypeDataForCreate();
-		reference.referenceType(referenceType);
-		reference.referenceID("RS1");
-		reference.masterID("M1");
-		reference.expiry();
-		reference.nativeSourceID("NSID1");
-		reference.details("details");
-		return reference;
-	}
-
-	private ReferenceType creatreferenceTypeDataForCreate() {
-		ReferenceType referenceType = new ReferenceType();
-		Audited Audited = createAuditDataForCreate();
-		referenceType.auditData(Audited);
-		referenceType.referenceTypeID("referenceSourceID");
-		referenceType.referenceName("referenceSourceName");
-		referenceType.referenceType("referenceSource");
-		return referenceType;
-	}
-
-	public ReferenceSource creatreferenceSourceDataForCreate() {
-		ReferenceSource referenceSource = new ReferenceSource();
-		Audited Audited = createAuditDataForCreate();
-		referenceSource.auditData(Audited);
-		referenceSource.referenceSourceID("referenceSourceID");
-		referenceSource.referenceSourceName("referenceSourceName");
-		return referenceSource;
-
-	}
-
-	public String deleteReferenceByID() {
-
-		return "RT1";
-	}
-
 }
