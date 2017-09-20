9434b19929d253fb1370fd841a702193f5016c46
snarthu <snarthu@DIN37001085.corp.capgemini.com>
2017-09-20 13:21:26 +0530

    Deleting files.

--- applications/crossreference-api/src/main/java/com/virginvoyages/dao/IReferenceTypesDAO.java
+++
@@ -1,9 +0,0 @@
-package com.virginvoyages.dao;
-
-import com.virginvoyages.crossreference.types.ReferenceType;
-
-public interface IReferenceTypesDAO {
-
-	public void addReferenceTypeToReferenceTypes(ReferenceType referenceType);
-
-}
