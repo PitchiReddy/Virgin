fe64820117ff19d66bb7e97edabf46b6590d74e7
pitchireddy <pitchireddy.bovilla@capgemini.com>
2017-09-22 17:59:11 +0530

    Implemented delete/update operations for ReferenceSource

--- applications/crossreference-api/src/main/java/com/virginvoyages/assembly/impl/ReferenceSourcesAssemblyImpl.java
+++ applications/crossreference-api/src/main/java/com/virginvoyages/assembly/impl/ReferenceSourcesAssemblyImpl.java
@@ -1,5 +1,7 @@
 package com.virginvoyages.assembly.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.virginvoyages.assembly.ReferenceSourcesAssembly;
@@ -41,6 +43,41 @@ public class ReferenceSourcesAssemblyImpl implements ReferenceSourcesAssembly {
 		log.debug("Entering findReferenceSourceByID method in ReferenceSourcesAssemblyImpl");
 		ReferenceSource getReferenceSource = referenceSourcesDAO.findReferenceSourceByID(referenceSourceID);
 		return getReferenceSource;
+	}
+	
+	 /**
+     * Remove the ReferenceSource. Dummy data being used as of now - as data source not finalized
+     * @param referenceSourceID - input referenceSourceID.
+     * @return
+    */
+	@Override
+	public void deleteReferenceSourceByID(String referenceSourceID) {
+		log.debug("Entering deleteReferenceSourceByID method in ReferenceSourcesAssemblyImpl");
+		referenceSourcesDAO.deleteReferenceSourceByID(referenceSourceID);
+	}
+
+	 /**
+     * Update a `ReferenceSource` object . Dummy data being used as of now - as data source not finalized
+     * @param referenceSourceID - input referenceSourceID.
+     * @param referenceSource - input referenceSource.
+     * @return
+    */
+	@Override
+	public void updateReferenceSource(String referenceSourceID, ReferenceSource referenceSource) {
+		log.debug("Entering updateReferenceSource method in ReferenceSourcesAssemblyImpl");
+		referenceSourcesDAO.updateReferenceSource(referenceSourceID,referenceSource);
+		
+	}
+
+	/**
+     * Gets `Source` objects. . Dummy data being used as of now - as data source not finalized
+     * @return List<ReferenceSource>
+    */
+	@Override
+	public List<ReferenceSource> findSources() {
+		log.debug("Entering findSources method in ReferenceSourcesAssemblyImpl");
+		List<ReferenceSource> listOfReferenceSources = referenceSourcesDAO.findSources();
+		return listOfReferenceSources;
 	}
 	
 }
