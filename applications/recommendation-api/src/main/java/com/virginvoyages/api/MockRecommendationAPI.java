1c9341669fd540b04f8234a3573727dfeee170a8
rpraveen <rpraveen@LIN66004745.corp.capgemini.com>
2017-09-26 18:49:44 +0530

    https://virginvoyages.atlassian.net/browse/PSS-1743 - Initial Check-in

--- applications/recommendation-api/src/main/java/com/virginvoyages/api/MockRecommendationAPI.java
+++ applications/recommendation-api/src/main/java/com/virginvoyages/api/MockRecommendationAPI.java
@@ -3,8 +3,12 @@ package com.virginvoyages.api;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
+import com.virginvoyages.recommendations.content.ContentRecommendation;
+import com.virginvoyages.recommendations.content.ContentRecommendations;
 import com.virginvoyages.recommendations.model.RecommendationData;
 
 /**
@@ -15,13 +19,37 @@ public class MockRecommendationAPI {
 	
 	private List<RecommendationData> recommendationResponses = new ArrayList<RecommendationData>();
 
-    public void addRecommendationResponse(Integer nbxUniqueKey, String recommendation, String selectionSentiment) {
+	private List<ContentRecommendation> contentRecommendationsData = new ArrayList<ContentRecommendation>();
+	
+	@PostConstruct
+    void init() {
+		addContentRecommendation(new ContentRecommendation()
+				.contentTag("Dummy Content Tag 1")
+				.contentTagID(1)
+				.nbxUniqueKey(123));
+		addContentRecommendation(new ContentRecommendation()
+				.contentTag("Dummy Content Tag 2")
+				.contentTagID(2)
+				.nbxUniqueKey(234));
+	
+    }
+	
+	public void addRecommendationResponse(Integer nbxUniqueKey, String recommendation, String selectionSentiment) {
     	recommendationResponses
     		.add(new RecommendationData()
     				.nbxUniqueKey(nbxUniqueKey)
     				.recommedation(recommendation)
     				.selectionSentiment(selectionSentiment));
     	
+    }
+    
+    public ContentRecommendations getContentRecommendations(String requestSource,String sailorID,
+    		String channel,List<String> place) {
+    	return new ContentRecommendations().contentReco(contentRecommendationsData);
+    }
+    
+    public void addContentRecommendation(ContentRecommendation contentRecommendation) {
+    	contentRecommendationsData.add(contentRecommendation);
     }
  
 	
