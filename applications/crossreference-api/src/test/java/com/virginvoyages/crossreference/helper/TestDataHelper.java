1ded56c55500f645e2c72f98acd47ab4e13a71dc
snarthu <snarthu@DIN37001085.corp.capgemini.com>
2017-10-17 18:01:21 +0530

    Implementing ReferenceRepository Test cases

--- applications/crossreference-api/src/test/java/com/virginvoyages/crossreference/helper/TestDataHelper.java
+++ applications/crossreference-api/src/test/java/com/virginvoyages/crossreference/helper/TestDataHelper.java
@@ -1,11 +1,17 @@
 package com.virginvoyages.crossreference.helper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.virginvoyages.crossreference.references.Reference;
 import com.virginvoyages.crossreference.sources.ReferenceSource;
 import com.virginvoyages.crossreference.types.ReferenceType;
+import com.virginvoyages.data.entities.ReferenceData;
 import com.virginvoyages.data.entities.ReferenceSourceData;
 import com.virginvoyages.data.entities.ReferenceTypeData;
+import com.virginvoyages.data.repositories.ReferenceRepository;
 
 /**
  * Helper class for testcases
@@ -15,6 +21,9 @@ import com.virginvoyages.data.entities.ReferenceTypeData;
  */
 @Service
 public class TestDataHelper {
+	
+	@Autowired
+	private ReferenceRepository referenceRepository;
 
 	public ReferenceSource getDataForCreateReferenceSource() {
 
@@ -56,14 +65,42 @@ public class TestDataHelper {
 	public ReferenceTypeData getReferenceTypeDataEntityForCreate() {
 		return new ReferenceTypeData()
 				.referenceType("Reservation_Testfindone")
-				.referenceTypeID("ignore");
-				//.referenceSourceData(new ReferenceSourceData().referenceSourceID(getReferenceSourceDataEntityForCreate().referenceSourceID()));
+				.referenceTypeID("ignore")
+				.referenceSourceData(new ReferenceSourceData().referenceSourceID(getReferenceSourceDataEntityForCreate().referenceSourceID()));
 	}
 	
 	public ReferenceTypeData getReferenceTypeDataEntityForUpdate() {
 		return new ReferenceTypeData()
-				.referenceType("Reservation_updated");
-				//.referenceTypeID("RT1");
-				//.referenceSourceData(new ReferenceSourceData().referenceSourceID(getReferenceSourceDataEntityForCreate().referenceSourceID()));
+				.referenceType("Reservation_updated")
+				.referenceTypeID("RT1")
+				.referenceSourceData(new ReferenceSourceData().referenceSourceID(getReferenceSourceDataEntityForCreate().referenceSourceID()));
+	}
+	
+	public ReferenceData getReferenceDataEntityForCreate() {
+		return new ReferenceData()
+				  .referenceID("R30")
+				  .nativeSourceIDValue("NSIDV30")
+				  .masterID("3")
+				  .referenceTypeData(new ReferenceTypeData().referenceTypeID(getReferenceTypeDataEntityForUpdate().referenceTypeID()));
+	}
+
+	public ReferenceData getReferenceDataEntityForUpdate() {
+		return new ReferenceData()
+				  .referenceID("R1")
+				  .nativeSourceIDValue("NSIDV30_updated")
+				  .masterID("M30_updated")
+				  .referenceTypeData(new ReferenceTypeData().referenceTypeID(getReferenceTypeDataEntityForUpdate().referenceTypeID()));
+	}
+
+	
+	public List<Reference> getReferenceDataBymaster(String masterID) {
+		Iterable<ReferenceData> referenceDataIterable = referenceRepository.findAll();
+		List<Reference> listOfReference = new ArrayList<>();
+		for (ReferenceData referenceData : referenceDataIterable) {
+			if((referenceData.masterID()).equals(masterID)) {
+			listOfReference.add(referenceData.convertToBusinessEntity());
+			}
+		}
+		return null == referenceDataIterable ? null : listOfReference;
 	}
 }
