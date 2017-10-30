139f3ef61e841f19616d20e84e165035c246a171
Bansal <amit.bansal@capgemini.com>
2017-10-30 11:27:42 +0530

    updated build.bat file

--- build.bat
+++ build.bat
@@ -3,16 +3,16 @@ set /p BUILD_NUMBER="Enter Build Number: "
 set /p name="Enter Name(sailor,reco,xref,all): "
 
 if %name%==sailor (
-gradle applications:sailor-api:buildDocker -xtest
+gradle applications:sailor-api:buildDocker -xtest -Ppush=false
 ) 
  if %name%==reco (
-gradle applications:recommendation-api:buildDocker -xtest
+gradle applications:recommendation-api:buildDocker -xtest -Ppush=false
 
 )
 if %name%==xref (
-gradle applications:crossreference-api:buildDocker -xtest
+gradle applications:crossreference-api:buildDocker -xtest -Ppush=false
 )
 
 if %name%==all (
-gradle build -xtest
+gradle build -xtest -Ppush=false
 )
