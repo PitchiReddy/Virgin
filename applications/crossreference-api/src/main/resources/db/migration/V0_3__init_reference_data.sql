40b9e80da12045a832a53616a1390daf964cf7bf
rpraveen <rpraveen@LIN66004745.corp.capgemini.com>
2017-11-10 15:20:15 +0530

    Removed duplicates, fixed insert for reference to be dynamic

--- applications/crossreference-api/src/main/resources/db/migration/V0_3__init_reference_data.sql
+++ applications/crossreference-api/src/main/resources/db/migration/V0_3__init_reference_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO REFERENCE VALUES ('1', 'cf378ee5-c07e-11e7-9541-0afa11e26204', 'test123', 'master123');
+INSERT INTO REFERENCE VALUES ('1', (select REFERENCE_TYPE_ID from REFERENCE_TYPE where REFERENCE_TYPE = 'WebProfile'), 'test123', 'master123');
 commit;
