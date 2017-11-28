52ba7e2d506a2f16fa5f494e33ad605df1e55f65
Narthu <sivashankar.narthu@capgemini.com>
2017-11-28 19:29:22 +0530

    pss-3104 implemented SeawareDao

--- applications/sailor-api/src/main/java/com/virginvoyages/seaware/dao/impl/SeawareDAOImpl.java
+++ applications/sailor-api/src/main/java/com/virginvoyages/seaware/dao/impl/SeawareDAOImpl.java
@@ -1,13 +1,18 @@
 package com.virginvoyages.seaware.dao.impl;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.virginvoyages.seaware.client.SeawareClient;
 import com.virginvoyages.seaware.dao.SeawareDAO;
 import com.virginvoyages.seaware.data.ClientData;
+import com.virginvoyages.seaware.data.CompanyNameType;
 import com.virginvoyages.seaware.data.OTAProfileReadRS;
 import com.virginvoyages.seaware.data.OTAReadRQ;
+import com.virginvoyages.seaware.data.POSType;
+import com.virginvoyages.seaware.data.SourceType;
 
 @Service
 public class SeawareDAOImpl implements SeawareDAO {
@@ -27,11 +32,68 @@ public class SeawareDAOImpl implements SeawareDAO {
 	}
 	
 	private ClientData convertResponseToSeawareClientDataObject(OTAProfileReadRS profileResponse) {
-		return new ClientData();
+	
+		ClientData clientData = new ClientData();
+	//	clientData.setSurname(profileResponse.getProfiles().getProfileInfo().stream().filter(pType -> pType.getUniqueID()));
+		clientData.setSuccess(profileResponse.getSuccess());
+		clientData.setWarnings(profileResponse.getWarnings());
+		clientData.setProfiles(profileResponse.getProfiles());
+		clientData.setErrors(profileResponse.getErrors());
+		clientData.setEchoToken(profileResponse.getEchoToken());
+		clientData.setTimeStamp(profileResponse.getTimeStamp());
+		clientData.setTarget(profileResponse.getTarget());
+		clientData.setVersion(profileResponse.getVersion());
+		clientData.setTargetName(profileResponse.getTargetName());
+		clientData.setTransactionIdentifier(profileResponse.getTransactionIdentifier());
+		clientData.setSequenceNmbr(profileResponse.getSequenceNmbr());
+		clientData.setTransactionStatusCode(profileResponse.getTransactionStatusCode());
+		clientData.setCorrelationID(profileResponse.getCorrelationID());
+		clientData.setRetransmissionIndicator(profileResponse.isRetransmissionIndicator());
+		clientData.setAltLangID(profileResponse.getAltLangID());
+		clientData.setPrimaryLangID(profileResponse.getPrimaryLangID());
+		            
+		return clientData;
+		//	return new ClientData();
 	}
 	
 	private OTAReadRQ createBaseSeawareReadRequest() {
-		return new OTAReadRQ();
+		OTAReadRQ otaReadRQ = new OTAReadRQ();
+		POSType posType = new POSType();
+	    SourceType sourceType = new SourceType();
+	    OTAReadRQ.ReadRequests readRequests = new OTAReadRQ.ReadRequests();
+	    OTAReadRQ.ReadRequests.ProfileReadRequest profileReadRequest =  new OTAReadRQ.ReadRequests.ProfileReadRequest();
+	    OTAReadRQ.ReadRequests.ProfileReadRequest.UniqueID uniqueID = new OTAReadRQ.ReadRequests.ProfileReadRequest.UniqueID();
+		SourceType.RequestorID  requestorID = new SourceType.RequestorID();
+		SourceType.BookingChannel bookingChannel = new SourceType.BookingChannel();
+		CompanyNameType companyNameType = new CompanyNameType();
+		companyNameType.setValue("OPENTRAVEL");
+	    uniqueID.setID("405");
+		uniqueID.setIDContext("SEAWARE");
+		uniqueID.setType("1");
+		profileReadRequest.getUniqueID().add(uniqueID);
+		readRequests.getProfileReadRequest().add(profileReadRequest);
+	
+		requestorID.setID("5");
+		requestorID.setType("5");
+		requestorID.setIDContext("SEAWARE");
+		
+		bookingChannel.setCompanyName(companyNameType);
+		bookingChannel.setType("1");
+		
+		
+		
+		sourceType.setRequestorID(requestorID);
+		
+		sourceType.setBookingChannel(bookingChannel);
+		posType.getSource().add(sourceType);
+		otaReadRQ.setPOS(posType);
+		otaReadRQ.setReadRequests(readRequests);
+		otaReadRQ.setPrimaryLangID("ENG");
+		otaReadRQ.setVersion(new BigDecimal(1));
+		//otaReadRQ.setXmlns("http://www.opentravel.org/OTA/2003/05");
+		otaReadRQ.setReadRequests(readRequests);
+		return otaReadRQ;
+	//	return new OTAReadRQ();
 	}
 	
 
--- applications/sailor-api/src/main/java/com/virginvoyages/seaware/data/ClientData.java
+++ applications/sailor-api/src/main/java/com/virginvoyages/seaware/data/ClientData.java
@@ -1,10 +1,140 @@
 package com.virginvoyages.seaware.data;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(fluent = true, chain = true)
 public class ClientData {
+	
+	protected SuccessType success;
+    protected WarningsType warnings;
+    protected List<UniqueIDType> uniqueID;
+    protected ProfilesType profiles;
+    protected ErrorsType errors;
+    protected String echoToken;
+    protected XMLGregorianCalendar timeStamp;
+    protected String target;
+    protected String targetName;
+    protected BigDecimal version;
+    protected String transactionIdentifier;
+    protected BigInteger sequenceNmbr;
+    protected String transactionStatusCode;
+    protected Boolean retransmissionIndicator;
+    protected String correlationID;
+    protected String primaryLangID;
+    protected String altLangID;
+    
+    
+    
+	public SuccessType getSuccess() {
+		return success;
+	}
+	public void setSuccess(SuccessType success) {
+		this.success = success;
+	}
+	public WarningsType getWarnings() {
+		return warnings;
+	}
+	public void setWarnings(WarningsType warnings) {
+		this.warnings = warnings;
+	}
+	public List<UniqueIDType> getUniqueID() {
+		return uniqueID;
+	}
+	public void setUniqueID(List<UniqueIDType> uniqueID) {
+		this.uniqueID = uniqueID;
+	}
+	public ProfilesType getProfiles() {
+		return profiles;
+	}
+	public void setProfiles(ProfilesType profiles) {
+		this.profiles = profiles;
+	}
+	public ErrorsType getErrors() {
+		return errors;
+	}
+	public void setErrors(ErrorsType errors) {
+		this.errors = errors;
+	}
+	public String getEchoToken() {
+		return echoToken;
+	}
+	public void setEchoToken(String echoToken) {
+		this.echoToken = echoToken;
+	}
+	public XMLGregorianCalendar getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(XMLGregorianCalendar timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public String getTarget() {
+		return target;
+	}
+	public void setTarget(String target) {
+		this.target = target;
+	}
+	public String getTargetName() {
+		return targetName;
+	}
+	public void setTargetName(String targetName) {
+		this.targetName = targetName;
+	}
+	public BigDecimal getVersion() {
+		return version;
+	}
+	public void setVersion(BigDecimal version) {
+		this.version = version;
+	}
+	public String getTransactionIdentifier() {
+		return transactionIdentifier;
+	}
+	public void setTransactionIdentifier(String transactionIdentifier) {
+		this.transactionIdentifier = transactionIdentifier;
+	}
+	public BigInteger getSequenceNmbr() {
+		return sequenceNmbr;
+	}
+	public void setSequenceNmbr(BigInteger sequenceNmbr) {
+		this.sequenceNmbr = sequenceNmbr;
+	}
+	public String getTransactionStatusCode() {
+		return transactionStatusCode;
+	}
+	public void setTransactionStatusCode(String transactionStatusCode) {
+		this.transactionStatusCode = transactionStatusCode;
+	}
+	public Boolean getRetransmissionIndicator() {
+		return retransmissionIndicator;
+	}
+	public void setRetransmissionIndicator(Boolean retransmissionIndicator) {
+		this.retransmissionIndicator = retransmissionIndicator;
+	}
+	public String getCorrelationID() {
+		return correlationID;
+	}
+	public void setCorrelationID(String correlationID) {
+		this.correlationID = correlationID;
+	}
+	public String getPrimaryLangID() {
+		return primaryLangID;
+	}
+	public void setPrimaryLangID(String primaryLangID) {
+		this.primaryLangID = primaryLangID;
+	}
+	public String getAltLangID() {
+		return altLangID;
+	}
+	public void setAltLangID(String altLangID) {
+		this.altLangID = altLangID;
+	}
+	
 	
 }
