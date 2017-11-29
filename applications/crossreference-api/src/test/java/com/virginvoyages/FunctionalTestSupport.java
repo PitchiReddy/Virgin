e97d0c356759102f6e7f4eddb3bcc9e41bf2ad1f
CORP\ambansal <amit.bansal@capgemini.com>
2017-11-29 14:27:29 +0530

    Added Oauth2 Suppor to Funtional test cases

--- applications/crossreference-api/src/test/java/com/virginvoyages/FunctionalTestSupport.java
+++ applications/crossreference-api/src/test/java/com/virginvoyages/FunctionalTestSupport.java
@@ -1,16 +1,19 @@
 package com.virginvoyages;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.port;
 
 import java.util.UUID;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,17 +22,21 @@ public abstract class FunctionalTestSupport {
     @LocalServerPort
     int LOCAL_PORT;
 
-    /*@BeforeClass
-    public static void globalSetup() {
-        port = LOCAL_PORT;
-    }*/
-
     @Before
     public void setUp() {
+    	
+    	@SuppressWarnings("deprecation")
+		final JsonPath jsonResponse = given()
+    			.contentType("application/json")
+    		.parameters("client_id", "4f177c76-1b53-4ad3-89b5-6c75d1745526", 
+    				"client_secret", "tDJJSjWS2hAawZd3", "grant_type", "client_credentials")
+    			.post("http://10.3.100.88:31362/svc/identityaccessmanagement-service/oauth/token").jsonPath();  	
+    	final String accessToken = jsonResponse.get("access_token");
+    	    	
     	port = LOCAL_PORT;
         RestAssured.given()
-                .header("correlationID", UUID.randomUUID().toString());
-        //specification.log().everything();
+                .headers("correlationID", UUID.randomUUID().toString(), "Authorization", "Bearer " + accessToken);
+        
     }
 
 }
